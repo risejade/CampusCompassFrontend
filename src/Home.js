61d36329b999666da10b4be9630a52bb41b78c2a
Rise Jade Benavente <143713851+risejade@users.noreply.github.com>
2023-11-18 21:40:11 +0800

    Update Home.js
    updated the dropdown button

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar, Typography, Menu, MenuItem, Button } from '@mui/material';
 import './CCcss/Home.css'; // Assuming you have a separate CSS file for styling
 import campusLogo from './CCcss/CCimage/campus.png';
 import hrLogo from './CCcss/CCimage/hrlogo.png';
@@ -136,7 +136,22 @@ function DashboardBox() {
 function Home() {
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
+  const [anchorEl, setAnchorEl] = useState(null);
 
+  const handleDropdownClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleDropdownClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleItemClick = (item) => {
+    // Logic for handling dropdown item clicks
+    console.log(`Clicked on: ${item}`);
+    handleDropdownClose(); // Close the dropdown after clicking an item
+    // You can add further logic based on the item clicked
+  };
   const handleLogin = () => {
     setUser();
     navigate('/app');
@@ -158,6 +173,9 @@ function Home() {
   const handleLandingPage = () => {
     navigate('/landingpage');
   };
+  const handleLogout = () => {
+    navigate('/landingpage');
+  };
 
   return (
     <div>
@@ -166,6 +184,7 @@ function Home() {
           <button className='thecampuslog' onClick={handleLandingPage}>
             <img src={campusLogo} alt="The Campus Logo" />
           </button>
+          <div className='appbar'>
           <div className='homebut' onClick={handleHome} style={{ cursor: 'pointer' }}>
             <Typography variant="button">
               Home
@@ -176,10 +195,35 @@ function Home() {
               About Us
             </Typography>
           </div>
-          <div className='services' onClick={handleContact} style={{ cursor: 'pointer' }}>
-            <Typography variant="button">
+          <div className='services' style={{ cursor: 'pointer' }}>
+            <Button onClick={handleDropdownClick} style={{ color: 'black' }}>
               Services
+            </Button>
+            <Menu
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={handleDropdownClose}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              getContentAnchorEl={null}
+            >
+              <MenuItem onClick={() => handleItemClick('Search Building')}>Search Building</MenuItem>
+              <MenuItem onClick={() => handleItemClick('Building Information')}>Building Information</MenuItem>
+              <MenuItem onClick={() => handleItemClick('Maintenance Report')}>Maintenance Report</MenuItem>
+              <MenuItem onClick={() => handleItemClick('Events')}>Events</MenuItem>
+            </Menu>
+          </div>
+          <div className='logout' onClick={handleLogout} style={{ cursor: 'pointer' }}>
+            <Typography variant="button">
+              Log out
             </Typography>
+          </div>
           </div>
         </Toolbar>
       </AppBar>
