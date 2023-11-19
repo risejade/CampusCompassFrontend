e8843b2e7e3de21b0f4daecbe958706349a7213f
Thesaly Tejano <thesaly.tejano@cit.edu>
2023-11-19 21:23:42 +0800

    origin

--- src/BuildingInformation.js
+++ src/BuildingInformation.js
@@ -7,210 +7,210 @@ import GLE from './CCcss/CCimage/GLE.jpg';
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Baloo+Chettan:wght@400;700&display=swap" />
 
 function BuildingInformation() {
-  const navigate = useNavigate();
-  const [user, setUser] = useState(null);
-  const [anchorEl, setAnchorEl] = useState(null);
+const navigate = useNavigate();
+const [user, setUser] = useState(null);
+const [anchorEl, setAnchorEl] = useState(null);
 
-  const handleDropdownClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+const handleDropdownClick = (event) => {
+  setAnchorEl(event.currentTarget);
+};
 
-  const handleDropdownClose = () => {
-    setAnchorEl(null);
-  };
+const handleDropdownClose = () => {
+  setAnchorEl(null);
+};
 
-  const handleItemClick = (item) => {
-    // Logic for handling dropdown item clicks
-    console.log(`Clicked on: ${item}`);
-    handleDropdownClose(); // Close the dropdown after clicking an item
-    // You can add further logic based on the item clicked
-  };
-  const handleLogin = () => {
-    setUser();
-    navigate('/app');
-  };
+const handleItemClick = (item) => {
+  // Logic for handling dropdown item clicks
+  console.log(`Clicked on: ${item}`);
+  handleDropdownClose(); // Close the dropdown after clicking an item
+  // You can add further logic based on the item clicked
+};
+const handleLogin = () => {
+  setUser();
+  navigate('/app');
+};
 
-  const handleHome = () => {
-    setUser();
-    navigate('/home');
-  };
+const handleHome = () => {
+  setUser();
+  navigate('/home');
+};
 
-  const handleAbout = () => {
-    navigate('/about');
-  };
+const handleAbout = () => {
+  navigate('/about');
+};
 
-  const handleContact = () => {
-    navigate('/contact');
-  };
+const handleContact = () => {
+  navigate('/contact');
+};
 
-  const handleLandingPage = () => {
-    navigate('/landingpage');
-  };
-  const handleLogout = () => {
-    navigate('/landingpage');
-  };
+const handleLandingPage = () => {
+  navigate('/landingpage');
+};
+const handleLogout = () => {
+  navigate('/landingpage');
+};
 
-  const Card = ({
-    imgSrc,
-    imgAlt,
-    title,
-    description,
-    buttonText,
-    link,
-  }) => {
-    return (
-      <div className="card-container">
-        {imgSrc && imgAlt && (
-          <img src={imgSrc} alt={imgAlt} className="card-img" />
-        )}
-        {title && <h1 className="card-title">{title}</h1>}
-        {description && <p className="card-description">{description}</p>}
-        {buttonText && link && (
-          <a href={link} className="card-btn">
-            {buttonText}
-          </a>
-        )}
-      </div>
-    );
-  };
-
+const Card = ({
+  imgSrc,
+  imgAlt,
+  title,
+  description,
+  buttonText,
+  link,
+}) => {
   return (
-    <div >
-      <AppBar position="relative" style={{ backgroundColor: 'rgba(246, 180, 96, 0.8)' }}>
-        <Toolbar variant="dense">
-          <button className='thecampuslog' onClick={handleLandingPage}>
-            <img src={campusLogo} alt="The Campus Logo" />
-          </button>
-          <div className='appbar'>
-          <div className='homebut' onClick={handleHome} style={{ cursor: 'pointer' }}>
+    <div className="card-container">
+      {imgSrc && imgAlt && (
+        <img src={imgSrc} alt={imgAlt} className="card-img" />
+      )}
+      {title && <h1 className="card-title">{title}</h1>}
+      {description && <p className="card-description">{description}</p>}
+      {buttonText && link && (
+        <a href={link} className="card-btn">
+          {buttonText}
+        </a>
+      )}
+    </div>
+  );
+};
+
+return (
+  <div >
+    <AppBar position="relative" style={{ backgroundColor: 'rgba(246, 180, 96, 0.8)' }}>
+      <Toolbar variant="dense">
+        <button className='thecampuslog' onClick={handleLandingPage}>
+          <img src={campusLogo} alt="The Campus Logo" />
+        </button>
+        <div className='appbar'>
+        <div className='homebut' onClick={handleHome} style={{ cursor: 'pointer' }}>
+          <Typography variant="button">
+            Home
+          </Typography>
+        </div>
+        <div className='about' onClick={handleAbout} style={{ cursor: 'pointer' }}>
+          <Typography variant="button">
+            About Us
+          </Typography>
+        </div>
+        <div className='services' style={{ cursor: 'pointer' }}>
+          <Button onClick={handleDropdownClick} style={{ color: 'white' }}>
+            Services
+          </Button>
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleDropdownClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            getContentAnchorEl={null}
+          >
+            <MenuItem onClick={() => handleItemClick('Search Building')}>Search Building</MenuItem>
+            <MenuItem onClick={(handleBuildingInformation) => handleItemClick('Building Information')}>Building Information</MenuItem>
+            <MenuItem onClick={() => handleItemClick('Maintenance Report')}>Maintenance Report</MenuItem>
+            <MenuItem onClick={() => handleItemClick('Events')}>Events</MenuItem>
+          </Menu>
+        </div>
+        
+          <div className='logout' onClick={handleLogout} style={{ cursor: 'pointer' }}>
             <Typography variant="button">
-              Home
+              Log out
             </Typography>
           </div>
-          <div className='about' onClick={handleAbout} style={{ cursor: 'pointer' }}>
-            <Typography variant="button">
-              About Us
-            </Typography>
-          </div>
-          <div className='services' style={{ cursor: 'pointer' }}>
-            <Button onClick={handleDropdownClick} style={{ color: 'white' }}>
-              Services
-            </Button>
-            <Menu
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
-              onClose={handleDropdownClose}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              getContentAnchorEl={null}
-            >
-              <MenuItem onClick={() => handleItemClick('Search Building')}>Search Building</MenuItem>
-              <MenuItem onClick={(handleBuildingInformation) => handleItemClick('Building Information')}>Building Information</MenuItem>
-              <MenuItem onClick={() => handleItemClick('Maintenance Report')}>Maintenance Report</MenuItem>
-              <MenuItem onClick={() => handleItemClick('Events')}>Events</MenuItem>
-            </Menu>
-          </div>
-          
-            <div className='logout' onClick={handleLogout} style={{ cursor: 'pointer' }}>
-              <Typography variant="button">
-                Log out
-              </Typography>
-            </div>
-          </div>
-        </Toolbar>
-      </AppBar>
-      
-      <div className="App">
-      <div className="col">
-        <Card
-          imgSrc={GLE} 
-          imgAlt="Card Image 1"
-          title="ACADEMIC BUILDING"
-          description="Academic Building Inaugurated in 2006, this building hosts the College of Engineering and Architecture.
-          "
-        />
-        <Card
-          imgSrc={GLE} 
-          imgAlt="Card Image 2"
-          title="ALLIED ENGINEERING"
-          description="Allied Engineering Building Situated behind the Main Building, these rooms are used mostly used for laboratory classes by the College of Engineering and Architecture.
-          "
-          buttonText="Read more"
-          link="card2"
-        />
-        <Card
-          imgSrc={GLE} 
-          imgAlt="Card Image 3"
-          title="EELEMENTARY BUILDING"
-          description="Elementary Building Opened in 1992, the building hosts the school's nursery school, kindergarten, and elementary department. It also hosts a playground open to the school's younger students."
-          buttonText="Read more"
-          link="card2"
-        />
-      </div>
-      <div className="col">
-        <Card
-          imgSrc={GLE} 
-          imgAlt="Card Image 1"
-          title="NGE"
-          description="Dr. Nicolas G. Escario, Sr. Building
-          This building was named in honor of Dr. Nicolas G. Escario, Sr., Founder and first President of Cebu Institute of Technology (CIT)"
-          buttonText="Read More"
-          link="card1"
-        />
-        <Card
-          imgSrc={GLE} 
-          imgAlt="Card Image 3"
-          title="GLE"
-          description="Gregorio L. Escario Building Opened in 2021, the new 8-storey building is situated in front of the Academic Building."
-          buttonText="Read More"
-          link="card2"
-        />
-        <Card
-          imgSrc={GLE} 
-          imgAlt="Card Image 3"
-          title="SAL"
-          description="Don Simplicio A. Lizares Building Opened in 1999, the building was originally called the High School building. In 2018, it was renamed as the Don Simplicio A. Lizares Building, to honor the school's first Chairman of the Board of Trustees."
-          link="card2"
-        />
-      </div>
-      <div className="col">
-        <Card
-          imgSrc={GLE}
-          imgAlt="Card Image 1"
-          title="P.E AREA"
-          buttonText="Read More"
-          link="card1"
-        />
-
-        <Card
-          imgSrc={GLE} 
-          imgAlt="Card Image 3"
-          title="RTL"
-          description="Don Rodolfo T. Lizares, Sr. Building
-          Opened in 1971, this building was the first to be built in the school's N. Bacalso campus. It is also known as the Main Building or the RTL Building"
-          buttonText="Read More"
-          link="card2"
-        />
-        <Card
-          imgSrc={GLE} 
-          imgAlt="Card Image 3"
-          title="SCHOOL GYMNASIUM"
-          description="CIT-U Gymnasium Constructed in 2004, this air-conditioned gymnasium is located behind the Don Simplicio A. Lizares Building and is where events and ceremonies are held. PE classes and other recreational activities also occur here."
-          buttonText="Read More"
-          link="card2"
-        />
-
-      </div>
-    </div>
-    </div>
+        </div>
+      </Toolbar>
+    </AppBar>
     
-    );
+    <div className="App">
+    <div className="col">
+      <Card
+        imgSrc={GLE} 
+        imgAlt="Card Image 1"
+        title="ACADEMIC BUILDING"
+        description="Academic Building Inaugurated in 2006, this building hosts the College of Engineering and Architecture.
+        "
+      />
+      <Card
+        imgSrc={GLE} 
+        imgAlt="Card Image 2"
+        title="ALLIED ENGINEERING"
+        description="Allied Engineering Building Situated behind the Main Building, these rooms are used mostly used for laboratory classes by the College of Engineering and Architecture.
+        "
+        buttonText="Read more about Engineering Department"
+        link="https://cit.edu/colleges-programs/cea/"
+      />
+      <Card
+        imgSrc={GLE} 
+        imgAlt="Card Image 3"
+        title="EELEMENTARY BUILDING"
+        description="Elementary Building Opened in 1992, the building hosts the school's nursery school, kindergarten, and elementary department. It also hosts a playground open to the school's younger students."
+        buttonText="Read more"
+        link="card2"
+      />
+    </div>
+    <div className="col">
+      <Card
+        imgSrc={GLE} 
+        imgAlt="Card Image 1"
+        title="NGE"
+        description="Dr. Nicolas G. Escario, Sr. Building
+        This building was named in honor of Dr. Nicolas G. Escario, Sr., Founder and first President of Cebu Institute of Technology (CIT)"
+        buttonText="Read More"
+        link="card1"
+      />
+      <Card
+        imgSrc={GLE} 
+        imgAlt="Card Image 3"
+        title="GLE"
+        description="Gregorio L. Escario Building Opened in 2021, the new 8-storey building is situated in front of the Academic Building."
+        buttonText="Read more about GLE building"
+        link="https://www.facebook.com/CITUniversity/posts/4671416402888677/"
+      />
+      <Card
+        imgSrc={GLE} 
+        imgAlt="Card Image 3"
+        title="SAL"
+        description="Don Simplicio A. Lizares Building Opened in 1999, the building was originally called the High School building. In 2018, it was renamed as the Don Simplicio A. Lizares Building, to honor the school's first Chairman of the Board of Trustees."
+        link="card2"
+      />
+    </div>
+    <div className="col">
+      <Card
+        imgSrc={GLE}
+        imgAlt="Card Image 1"
+        title="P.E AREA"
+        buttonText="Read More"
+        link="card1"
+      />
+
+      <Card
+        imgSrc={GLE} 
+        imgAlt="Card Image 3"
+        title="RTL"
+        description="Don Rodolfo T. Lizares, Sr. Building
+        Opened in 1971, this building was the first to be built in the school's N. Bacalso campus. It is also known as the Main Building or the RTL Building"
+        buttonText="Read More"
+        link="card2"
+      />
+      <Card
+        imgSrc={GLE} 
+        imgAlt="Card Image 3"
+        title="SCHOOL GYMNASIUM"
+        description="CIT-U Gymnasium Constructed in 2004, this air-conditioned gymnasium is located behind the Don Simplicio A. Lizares Building and is where events and ceremonies are held. PE classes and other recreational activities also occur here."
+        buttonText="Read More"
+        link="card2"
+      />
+
+    </div>
+  </div>
+  </div>
+  
+  );
 }
 
 export default BuildingInformation;
