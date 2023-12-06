4075064a0a7057a7f9460f219cc7ccc516915e7f
Thesaly Tejano <thesaly.tejano@cit.edu>
2023-12-06 14:11:22 +0800

    origin

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -1,43 +1,177 @@
-import React from 'react';
-import { Avatar, Typography, Button } from '@mui/material'; // You might need to import these components from Material-UI
+import axios from "axios";
+import React, {  useState, useEffect } from "react";
+import { IoPersonCircleOutline } from "react-icons/io5";
+import { toast } from "react-toastify";
+import {
+  Form,
+  Button,
+  FormGroup,
+  Input,
+  Label,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+} from "reactstrap";
+import "./CCcss/ProfilePage.css";
 
-const ProfilePage = () => {
-  // Sample user data (replace this with your actual user data)
-  const user = {
-    name: 'John Doe',
-    email: 'johndoe@example.com',
-    profileImage: '/CCcss/CCimage/rise.png', // Replace with the actual path to the user's profile image
-    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget lectus libero.',
-    // Add other user information as needed
+const UpoloadAvatar = ({
+  userId,
+  token,
+  username,
+  avatarUrl,
+  setisUserUpdated,
+}) => {
+  const [modal, setModal] = useState(false);
+  const [file, setFile] = useState(null);
+
+  const toggle = () => {
+    setModal(!modal);
   };
 
-  // Function to handle updating profile information
-  const handleUpdateProfile = () => {
-    // Logic for updating the user profile (e.g., opening a modal or navigating to an edit profile page)
-    console.log('Update profile clicked');
+  const handleFileChange = ({ target: { files } }) => {
+    if (files?.length) {
+      const { type } = files[0];
+      if (type === "image/png" || type === "image/jpeg") {
+        setFile(files[0]);
+      } else {
+        toast.error("Accept only png and jpeg image types are allowed*", {
+          hideProgressBar: true,
+        });
+      }
+    }
+  };
+
+  const upateUserAvatarId = async (avatarId, avatarUrl) => {
+    try {
+      await axios.put(
+        `http://localhost:8080/usercampus/update${userId}`,
+        { avatarId, avatarUrl },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `bearer ${token}`,
+          },
+        }
+      );
+      setisUserUpdated(true);
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
+  const handleSubmit = async () => {
+    if (!file) {
+      toast.error("File is required*", {
+        hideProgressBar: true,
+      });
+      return;
+    }
+
+    try {
+      const files = new FormData();
+      files.append("files", file);
+      files.append("name", `${username} avatar`);
+
+      const {
+        data: [{ id, url }],
+      } = await axios.post(`http://localhost:3000/api/upload`, files, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `bearer ${token}`,
+        },
+      });
+      upateUserAvatarId(id, url);
+      setFile(null);
+      setModal(false);
+    } catch (error) {
+      console.log({ error });
+    }
   };
 
   return (
-    <div className="profile-page">
-      <div className="profile-header">
-        <Avatar alt={user.name} src={user.profileImage} />
-        <div className="profile-info">
-          <Typography variant="h4">{user.name}</Typography>
-          <Typography variant="subtitle1">{user.email}</Typography>
-          {/* Additional user information (bio, location, etc.) */}
-          <Typography variant="body1">{user.bio}</Typography>
-          {/* Add more user details here if needed */}
-        </div>
-      </div>
-      <div className="profile-actions">
-        <Button variant="contained" color="primary" onClick={handleUpdateProfile}>
-          Update Profile
-        </Button>
-        {/* You can add more buttons for other actions */}
-      </div>
-      {/* Other sections of the profile page (e.g., user posts, settings, etc.) */}
+    <div>
+      <Button size="sm" onClick={toggle}>
+        {`${avatarUrl ? "Change" : "Upload"} picture`}
+      </Button>
+      <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle}>{`${
+          avatarUrl ? "Change" : "Upload"
+        } your avatar`}</ModalHeader>
+        <ModalBody>
+          <Form>
+            <FormGroup>
+              <Label for="exampleFile">File</Label>
+              <Input
+                type="file"
+                name="file"
+                id="exampleFile"
+                onChange={handleFileChange}
+              />
+            </FormGroup>
+          </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={handleSubmit}>
+            Upload
+          </Button>
+          <Button color="secondary" onClick={toggle}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
     </div>
   );
 };
 
-export default ProfilePage;
+const Profile = ({ token }) => {
+  const [user, setUser] = useState({});
+  const [isUserUpdated, setisUserUpdated] = useState(false);
+
+  useEffect(() => {
+    const getProfileData = async () => {
+      try {
+        const { data } = await axios.get(`http://localhost:8080/usercampus/getAllUsercampus`, {
+          headers: {
+            Authorization: `bearer ${token}`,
+          },
+        });
+        setUser(data);
+        setisUserUpdated(false);
+      } catch (error) {
+        console.log({ error });
+      }
+    };
+    getProfileData();
+  }, [token, isUserUpdated]);
+
+  return (
+    <div className="userprofile">
+      <div className="avatar">
+        <div className="avatar-wrapper">
+          {user.avatarUrl ? (
+            <img
+              src={`http://localhost:3000${user.avatarUrl}`}
+              alt={`${user.username} avatar`}
+            />
+          ) : (
+            <IoPersonCircleOutline />
+          )}
+          <UpoloadAvatar
+            token={token}
+            userId={user.id}
+            username={user.username}
+            avatarUrl={user.avatarUrl}
+            setisUserUpdated={setisUserUpdated}
+          />
+        </div>
+      </div>
+      <div className="profilebody">
+        <p>Name: {user.username}</p>
+        <p>Email: {user.email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
